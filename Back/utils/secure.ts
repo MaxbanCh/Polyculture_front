d3d9b78c51553f9a433493008fe219af64b9efe1
Maxence-Esteban CHATEAU <maxence-esteban.chateau@etu.umontpellier.fr>
2025-05-08 12:15:50 +0200

    Reorganisation pour deploiement sur dokku

--- Back/utils/secure.ts
+++
@@ -1,7 +0,0 @@
-import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
-
-export async function get_hash(password: string): Promise<string> {
-  const saltRounds = 10;
-  const salt = await bcrypt.genSalt(saltRounds);
-  return await bcrypt.hash(password, salt);
-}
